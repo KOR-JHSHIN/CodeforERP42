c15855b0d24ec26dbb48d488f76843b32c88c4ad
JEONGHWAN-SHIN <117723921+KOR-JHSHIN@users.noreply.github.com>
2024-07-12 12:10:22 +0900

    Update SavingWaypoints.py

--- SavingWaypoints.py
+++ SavingWaypoints.py
@@ -5,8 +5,9 @@ from sensor_msgs.msg import NavSatFix
 import os
 import csv
 from math import sqrt
+from pynput import keyboard
 
-class WaypointsSaving:
+class SavingWaypoints:
     def __init__(self):
         rospy.init_node("saving_waypoints")
         self.gps_sub = rospy.Subscriber("/ublox_gps/fix", NavSatFix, self.waypointsaving_callback)
@@ -14,6 +15,17 @@ class WaypointsSaving:
         self.csv_file = None
         file = open("gps_coordinates.txt", 'w')
         self.csv_file = csv.writer(file)
+        self.save_waypoint = False  
+
+        listener = keyboard.Listener(on_press=self.on_key_event)
+        listener.start()
+
+    def on_key_event(self, key):
+        try:
+            if key.char == 's':
+                self.save_waypoint = True
+        except AttributeError:
+            pass
 
     def waypointsaving_callback(self, msg):
         gps_msg = msg
@@ -29,26 +41,18 @@ class WaypointsSaving:
         x = coord[0]
         y = coord[1]
 
-        if self.previous_location is not None:
-         lf.previous_location[1])**2) * 111000
-            distance = sqrt((x - self.previous_location[0])**2 + (y - self.previous_location[1])**2)
-
-            if distance >= 0.000045: # 5미터 정도 이동시 waypoints 저장 
-                data = [x, y]
-                self.csv_file.writerow(data)
-                print(f"location : {data}")
-                self.previous_location = [x, y]
-            else:
-                print(f"location not moved enough to record: {x, y}")
-        else:
+        if self.save_waypoint:
             data = [x, y]
             self.csv_file.writerow(data)
-            print(f"initial location : {data}")
+            print(f"location saved: {data}")
             self.previous_location = [x, y]
+            self.save_waypoint = False
+        else:
+            print(f"location not saved: {x, y}")
 
 if __name__ == "__main__":
     try:
-        waypoints_saving = WaypointsSaving()
+        saving_waypoints = SavingWaypoints()
         rospy.spin()
     except rospy.ROSInterruptException:
         pass
